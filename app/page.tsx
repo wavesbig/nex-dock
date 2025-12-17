4c6327b757f10616e4afd1df4bba8beeaddc1c3d
郑涛 <wavesbig@qq.com>
2025-12-17 15:13:35 +0800

    feat: shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -59,6 +60,8 @@ export default function Home() {
             Documentation
           </a>
         </div>
+
+        <Button>Click me</Button>
       </main>
     </div>
   );
